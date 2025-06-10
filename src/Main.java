f64ec33049d350ebbfb8bac9fa442e342d0677fc
Alzbeta V. <alzbeta.voldanova@gmail.com>
2025-06-10 14:11:25 +0200

    Added getters and setters

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,21 @@ public class Main {
         Seller seller1 = new Seller();
         Seller seller2 = new Seller();
 
-        seller1.name = "Lukas Zajic";
-        seller1.dateOfBirth = LocalDate.of(1989, 10, 2);
+//        seller1.name = "Lukas Zajic";
+//        seller1.dateOfBirth = LocalDate.of(1989, 10, 2);
+//
+//        seller2.name = "Tom Celer";
+//        seller2.dateOfBirth = LocalDate.of(1975, 2, 11);
 
-        seller2.name = "Tom Celer";
-        seller2.dateOfBirth = LocalDate.of(1975, 2, 11);
+        seller1.setName("Lukas Zajic");
+        seller1.setDateOfBirth(LocalDate.of(1989, 10, 2));
+
+        seller2.setName("Tom Celer");
+        seller2.setDateOfBirth(LocalDate.of(1975, 2, 11));
 
 
-        System.out.println("Prodejce " + seller1.name + " se narodil " + seller1.dateOfBirth);
-        System.out.println("Prodejce " + seller2.name + " se narodil " + seller2.dateOfBirth);
+
+        System.out.println("Prodejce " + seller1.getName() + " se narodil " + seller1.getDateOfBirth());
+        System.out.println("Prodejce " + seller2.getName() + " se narodil " + seller2.getDateOfBirth());
     }
 }
--- src/Seller.java
+++ src/Seller.java
@@ -7,4 +7,44 @@ public class Seller {
     int signedContractsCount;
     double carrotSalesInTons;
     boolean isSeniorSeller;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getSignedContractsCount() {
+        return signedContractsCount;
+    }
+
+    public void setSignedContractsCount(int signedContractsCount) {
+        this.signedContractsCount = signedContractsCount;
+    }
+
+    public double getCarrotSalesInTons() {
+        return carrotSalesInTons;
+    }
+
+    public void setCarrotSalesInTons(double carrotSalesInTons) {
+        this.carrotSalesInTons = carrotSalesInTons;
+    }
+
+    public boolean isSeniorSeller() {
+        return isSeniorSeller;
+    }
+
+    public void setSeniorSeller(boolean seniorSeller) {
+        isSeniorSeller = seniorSeller;
+    }
 }
